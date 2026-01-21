ec0f83834bb78cceaf513258a86da73f7730bf96
yourimv <youridevlam@outlook.com>
2026-01-21 01:44:02 +0100

    Add pip install command to setup scripts for environment dependencies

--- run_linux.sh
+++ run_linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source bot-env/bin/activate
+pip install -r requirements.txt
 
 nohup python soundboard_web.py > soundboard_web.log 2>&1 &
 nohup python main.py > main.log 2>&1 &
--- run_windows.bat
+++ run_windows.bat
@@ -1,13 +1,17 @@
 @echo off
 REM Activate the virtual environment
+
 call "%~dp0bot-env\Scripts\activate.bat"
+python -m pip install -r requirements.txt
 
 REM Start the bot in a new window
 title sjefbot-bot
+
 start cmd /k python main.py
 
 REM Start the web server in a new window
 title sjefbot-web
+
 start cmd /k python -m uvicorn soundboard_web:app --host 127.0.0.1 --port 8080
 
 echo Both bot and web server started in separate windows.

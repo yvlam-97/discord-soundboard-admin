c1c8b9411c9b26f1979f82d0a3e3aa7672618fc1
yourimv <youridevlam@outlook.com>
2026-01-21 01:53:54 +0100

    Fix nextsound command to use DB_PATH for sound retrieval

--- commands/nextsound.py
+++ commands/nextsound.py
@@ -1,11 +1,12 @@
-
+import os
 from discord import app_commands, Interaction
 from tasks.voice_soundboard import get_time_until_next_sound
 
+DB_PATH = os.getenv("SOUNDBOARD_DB_PATH", os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), "soundboard.db")))
 
 @app_commands.command(name="nextsound", description="Show time left until next sound is played.")
 async def nextsound_command(interaction: Interaction):
-    time_left = get_time_until_next_sound()
+    time_left = get_time_until_next_sound(DB_PATH)
     if time_left == 0:
         await interaction.response.send_message("A sound can be played now!")
     else:
